f6c261a78b467f0d6b481bc23e5daea35919ea1e
Fred <fred@api4.me>
2015-08-17 06:35:29 +0800

    add available param for mysql curd

--- mysql.go
+++ mysql.go
@@ -2,9 +2,8 @@ package next
 
 import (
 	"database/sql"
-	"errors"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"log"
 	"reflect"
 	"regexp"
 	"runtime"
@@ -38,9 +37,67 @@ func (mysql *Mysql) Close() {
 	mysql.Db.Close()
 }
 
+func (mysql *Mysql) Query(args ...interface{}) (interface{}, error) {
+	// Parse sql
+	q, param, err := mysql.parseQuery(args...)
+	if err != nil {
+		return nil, err
+	}
+
+	var stmt *sql.Stmt
+	// Prepare
+	if mysql.Tx != nil {
+		stmt, err = mysql.Tx.Prepare(q)
+	} else {
+		stmt, err = mysql.Db.Prepare(q)
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	// -------------
+	// Scan
+	// -------------
+	rows, err := stmt.Query(param...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Get all col name
+	cols, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+
+	// Scan value
+	out := make([]interface{}, 0)
+	for rows.Next() {
+		rbs := make([]sql.RawBytes, len(cols))
+		vals := make([]interface{}, len(cols))
+		for i := range vals {
+			vals[i] = &rbs[i]
+		}
+
+		if err = rows.Scan(vals...); err != nil {
+			return nil, err
+		}
+
+		// Make map
+		r := make(map[string]interface{}, len(cols))
+		for i, val := range rbs {
+			r[cols[i]] = val
+		}
+		out = append(out, r)
+	}
+
+	return out, nil
+}
+
 func (mysql *Mysql) Row(args ...interface{}) (interface{}, error) {
 	// Parse sql
-	q, param, err := mysql.parse(args...)
+	q, param, err := mysql.parseQuery(args...)
 	if err != nil {
 		return nil, err
 	}
@@ -94,90 +151,25 @@ func (mysql *Mysql) Row(args ...interface{}) (interface{}, error) {
 	return out, nil
 }
 
-func (mysql *Mysql) Query(args ...interface{}) (interface{}, error) {
-	// Parse sql
-	q, param, err := mysql.parse(args...)
-	if err != nil {
-		return nil, err
+// DML for tabel, insert update delete operation.
+// usage:
+// mysql.Exec("users", "add", &addData)
+// mysql.Exec("users", "up", &upData, &where)
+// mysql.Exec("users", "del", &where)
+func (mysql *Mysql) Exec(table, dml string, args ...interface{}) (interface{}, error) {
+	if len(table) == 0 {
+		panic("plase set table for exec method")
 	}
 
-	var stmt *sql.Stmt
-	// Prepare
-	if mysql.Tx != nil {
-		stmt, err = mysql.Tx.Prepare(q)
-	} else {
-		stmt, err = mysql.Db.Prepare(q)
-	}
-	if err != nil {
-		return nil, err
-	}
-	defer stmt.Close()
-
-	// -------------
-	// Scan
-	// -------------
-	rows, err := stmt.Query(param...)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	// Get all col name
-	cols, err := rows.Columns()
-	if err != nil {
-		return nil, err
-	}
-
-	// Scan value
-	out := make([]interface{}, 0)
-	for rows.Next() {
-		rbs := make([]sql.RawBytes, len(cols))
-		vals := make([]interface{}, len(cols))
-		for i := range vals {
-			vals[i] = &rbs[i]
-		}
-
-		if err = rows.Scan(vals...); err != nil {
-			return nil, err
-		}
-
-		// Make map
-		r := make(map[string]interface{}, len(cols))
-		for i, val := range rbs {
-			r[cols[i]] = val
-		}
-		out = append(out, r)
-	}
-
-	log.Print(out)
-	return out, nil
-}
-
-func (mysql *Mysql) Exec(table, dml string, data map[string]interface{}) (interface{}, error) {
-	//
-	switch strings.ToLower(dml); {
-	case dml == "add":
-		/*
-			stmt, err := db.Prepare("INSERT INTO users(name) VALUES(?)")
-			if err != nil {
-			log.Fatal(err)
-			}
-			res, err := stmt.Exec("Dolly")
-			if err != nil {
-			log.Fatal(err)
-			}
-			lastId, err := res.LastInsertId()
-			if err != nil {
-			log.Fatal(err)
-			}
-			rowCnt, err := res.RowsAffected()
-			if err != nil {
-			log.Fatal(err)
-			}
-			log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
-		*/
-	case dml == "up":
-	case dml == "del":
+	switch strings.ToLower(dml); dml {
+	case "add":
+		return mysql.insert(table, args...)
+	case "up":
+		return mysql.update(table, args...)
+	case "del":
+		return mysql.del(table, args...)
+	default:
+		panic("mysql exec method <add, up, del> can not support: " + dml)
 	}
 	return nil, nil
 }
@@ -204,11 +196,11 @@ func (mysql *Mysql) Commit() error {
 	return nil
 }
 
-func (mysql *Mysql) parse(args ...interface{}) (string, []interface{}, error) {
+func (mysql *Mysql) parseQuery(args ...interface{}) (string, []interface{}, error) {
 	// SQL
 	q, ok := (args[0]).(string)
 	if !ok {
-		return "", []interface{}{}, errors.New("first param should be a sql string")
+		panic("first param should be a sql string")
 	}
 
 	// No param
@@ -219,7 +211,7 @@ func (mysql *Mysql) parse(args ...interface{}) (string, []interface{}, error) {
 	// Parse sql with mapping
 	v := reflect.ValueOf(args[1])
 	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Map {
-		return "", []interface{}{}, errors.New("param should be a map's pointer")
+		panic("param should be a map's pointer")
 	}
 
 	param := make([]interface{}, 0)
@@ -230,4 +222,150 @@ func (mysql *Mysql) parse(args ...interface{}) (string, []interface{}, error) {
 	})
 
 	return q, param, nil
+}
+
+func (mysql *Mysql) insert(table string, args ...interface{}) (interface{}, error) {
+	v := reflect.ValueOf(args[0])
+	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Map {
+		panic("param should be a map's pointer")
+	}
+
+	// Parse args
+	maps := v.Elem().MapKeys()
+	feild := make([]string, len(maps))
+	bind := make([]string, len(maps))
+	param := make([]interface{}, len(maps))
+	for i, k := range maps {
+		feild[i] = fmt.Sprintf("`%s`", k)
+		bind[i] = "?"
+		param[i] = v.Elem().MapIndex(k).Interface()
+	}
+	q := fmt.Sprintf("INSERT INTO `%s`(%s) VALUES(%s);", table, strings.Join(feild, ","), strings.Join(bind, ","))
+
+	// Sql execute
+	var stmt *sql.Stmt
+	var err error
+	// Prepare
+	if mysql.Tx != nil {
+		stmt, err = mysql.Tx.Prepare(q)
+	} else {
+		stmt, err = mysql.Db.Prepare(q)
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(param...)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.LastInsertId()
+}
+
+func (mysql *Mysql) update(table string, args ...interface{}) (interface{}, error) {
+	if len(args) < 2 {
+		panic("mysql update method need the data of update and where")
+	}
+
+	u := reflect.ValueOf(args[0])
+	if u.Kind() != reflect.Ptr || u.Elem().Kind() != reflect.Map {
+		panic("updata args should be a map's pointer")
+	}
+	w := reflect.ValueOf(args[1])
+	if w.Kind() != reflect.Ptr || w.Elem().Kind() != reflect.Map {
+		panic("where args should be a map's pointer")
+	}
+
+	param := make([]interface{}, 0)
+	// Parse upate args
+	mu := u.Elem().MapKeys()
+	up := make([]string, len(mu))
+	for i, k := range mu {
+		f := strings.SplitN(k.String(), ":", 2)
+		if len(f) > 1 {
+			up[i] = fmt.Sprintf("`%s`%s%s", f[0], f[1], u.Elem().MapIndex(k).Interface())
+		} else {
+			up[i] = fmt.Sprintf("`%s`=?", k)
+			param = append(param, u.Elem().MapIndex(k).Interface())
+		}
+	}
+	// Parse upate args
+	mw := w.Elem().MapKeys()
+	where := make([]string, len(mw))
+	for i, k := range mw {
+		f := strings.SplitN(k.String(), ":", 2)
+		if len(f) > 1 {
+			where[i] = fmt.Sprintf("`%s`%s%s", f[0], f[1], w.Elem().MapIndex(k).Interface())
+		} else {
+			where[i] = fmt.Sprintf("`%s`=?", k)
+			param = append(param, w.Elem().MapIndex(k).Interface())
+		}
+	}
+	q := fmt.Sprintf("UPDATE `%s` SET %s WHERE %s;", table, strings.Join(up, ","), strings.Join(where, " AND "))
+
+	// Sql execute
+	var stmt *sql.Stmt
+	var err error
+	// Prepare
+	if mysql.Tx != nil {
+		stmt, err = mysql.Tx.Prepare(q)
+	} else {
+		stmt, err = mysql.Db.Prepare(q)
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(param...)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.RowsAffected()
+}
+
+func (mysql *Mysql) del(table string, args ...interface{}) (interface{}, error) {
+	w := reflect.ValueOf(args[0])
+	if w.Kind() != reflect.Ptr || w.Elem().Kind() != reflect.Map {
+		panic("where args should be a map's pointer")
+	}
+
+	mw := w.Elem().MapKeys()
+	param := make([]interface{}, 0)
+	// Parse where args
+	where := make([]string, len(mw))
+	for i, k := range mw {
+		f := strings.SplitN(k.String(), ":", 2)
+		if len(f) > 1 {
+			where[i] = fmt.Sprintf("`%s`%s%s", f[0], f[1], w.Elem().MapIndex(k).Interface())
+		} else {
+			where[i] = fmt.Sprintf("`%s`=?", k)
+			param = append(param, w.Elem().MapIndex(k).Interface())
+		}
+	}
+	q := fmt.Sprintf("DELETE FROM `%s` WHERE %s;", table, strings.Join(where, " AND "))
+
+	// Sql execute
+	var stmt *sql.Stmt
+	var err error
+	// Prepare
+	if mysql.Tx != nil {
+		stmt, err = mysql.Tx.Prepare(q)
+	} else {
+		stmt, err = mysql.Db.Prepare(q)
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(param...)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.RowsAffected()
 }
